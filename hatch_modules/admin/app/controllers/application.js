76a4525942a6d2de0609bc5e30887fb73b05bce2
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-02-13 04:51:41 +0800

    Tabs and paths

--- hatch_modules/admin/app/controllers/application.js
+++ hatch_modules/admin/app/controllers/application.js
@@ -2,6 +2,13 @@ var Application = module.exports = function Application(init) {
     init.before(function initApp(c) {
         this.pageName = c.actionName;
         this.req = c.req;
+        this.tabs = [
+            { name: 'Community', url: 'community', rank: 10 },
+            { name: 'Content', url: 'content', rank: 20 },
+            { name: 'Pages', url: 'pages', rank: 30 },
+            { name: 'Settings', url: 'group', rank: 40 },
+            { name: 'Modules', url: 'modules', rank: 50 }
+        ]
         c.next();
     });
 };
