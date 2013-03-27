a2508f1e84317e1c7af8a3cbaa7ce96dbe411423
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-28 02:01:09 +0400

    REtab routes

--- hatch_modules/admin/config/routes.js
+++ hatch_modules/admin/config/routes.js
@@ -6,40 +6,38 @@ exports.routes = function (map) {
     map.resources('groups', function (group) {
 
     });
-    
-    //map.resources('groups', function (group) {
-        map.resources('modules', function (module) {
-            module.collection(function (modules) {
-                modules.get('marketplace', {as: 'groupModulesMarketplace'});
-            });
-            module.get('disable');
-            module.get('enable');
+
+    map.resources('modules', function (module) {
+        module.collection(function (modules) {
+            modules.get('marketplace', {as: 'groupModulesMarketplace'});
         });
-        map.resources('content', {as: 'content', suffix: 'entry'}, function (item) {
-            item.collection(function (items) {
-                items.get('filter/:filter.:format?', '#index', {as: 'filteredGroupContent' });
-                items.del('destroyAll', {as: 'destroySelectedGroupContent'});
-            });
+        module.get('disable');
+        module.get('enable');
+    });
+    map.resources('content', {as: 'content', suffix: 'entry'}, function (item) {
+        item.collection(function (items) {
+            items.get('filter/:filter.:format?', '#index', {as: 'filteredGroupContent' });
+            items.del('destroyAll', {as: 'destroySelectedGroupContent'});
         });
-        map.resources('tags', function (tag) {
-            tag.get('count');
+    });
+    map.resources('tags', function (tag) {
+        tag.get('count');
+    });
+    map.resources('streams', function (stream) {
+        stream.get('toggle');
+    });
+    map.resources('users', {as: 'community', suffix: 'member'});
+    map.resources('pages', function (page) {
+        page.collection(function (pages) {
+            pages.get('new-special', '#newSpecial', {as: 'newSpecial'});
+            pages.get('new-special/:type', '#newSpecial', {as: 'newSpecialType'});
+            pages.get('specials', '#specials', {as: 'specialPages'});
+            pages.get('renderTree', '#renderPageTree', {as: 'renderTree'});
+            pages.get('edit/:id', '#edit', {as: 'editPage'});
         });
-        map.resources('streams', function (stream) {
-            stream.get('toggle');
-        });
-        map.resources('users', {as: 'community', suffix: 'member'});
-        map.resources('pages', function (page) {
-            page.collection(function (pages) {
-                pages.get('new-special', '#newSpecial', {as: 'newSpecial'});
-                pages.get('new-special/:type', '#newSpecial', {as: 'newSpecialType'});
-                pages.get('specials', '#specials', {as: 'specialPages'});
-                pages.get('renderTree', '#renderPageTree', {as: 'renderTree'});
-                pages.get('edit/:id', '#edit', {as: 'editPage'});
-            });
-            page.put('reorder.:format?', 'pages#updateOrder');
-            
-        });
-    //});
+        page.put('reorder.:format?', 'pages#updateOrder');
+        
+    });
 
     map.post('/page/columns', 'page#updateColumns');
 
