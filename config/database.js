8ff0a88e52c40b0e31e91eb9d27b9afbf74063e6
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-28 02:00:34 +0400

    Added db prefixes to db conf

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 module.exports = 
   { "development":
     { "driver":   "redis-hq"
+    , "prefix":   "dev"
     }
   , "test":
     { "driver":   "redis-hq"
+    , "prefix":   "test"
     }
   , "production":
     { "driver":   "redis-hq"
+    , "prefix":   "prod"
     }
   };
