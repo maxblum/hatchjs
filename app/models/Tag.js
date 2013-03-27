9efcf60bb6fc3ec962396df160beda17fcfd8793
Marcus Greenwood <marcusgreenwood@Marcuss-MacBook-Air.local>
2013-03-27 11:17:12 +0000

    jslint'ing the Tag.js

--- app/models/Tag.js
+++ app/models/Tag.js
@@ -19,55 +19,60 @@
 var async = require('async');
 
 module.exports = function (compound, Tag) {
+    'use strict';
     var api = compound.hatch.api;
 
     Tag.validatesPresenceOf('title', {message: 'Please enter a title'});
     Tag.validatesUniquenessOf('name', {message: 'This tag name is taken'});
 
     /**
-     * updates the compound model for the specified tag to make sure the custom
-     * sort order is defined for the specified tag
+     * Update the compound model for the specified tag to make sure the custom
+     * sort order is defined for the specified tag.
      *
      * This is needed before any items that use this tag can be saved so that 
-     * the sort orders are correct when they are queried by tag
+     * the sort orders are correct when they are queried by tag.
      * 
-     * @param  {Tag} tag [tag to check for]
+     * @param  {Tag} tag - tag to check for
      */
     Tag.updateModel = function (tag) {
-        if(!tag.sortOrder) return;
+        if (!tag.sortOrder) {
+            return;
+        }
 
         var settings = compound.models[tag.type].schema.settings;
-        if(!settings.customSort) settings.customSort = {};
+        if (!settings.customSort) {
+            settings.customSort = {};
+        }
 
-        if(!settings.customSort['tags.' + tag.name]) {
+        if (!settings.customSort['tags.' + tag.name]) {
             settings.customSort['tags.' + tag.name] = tag.sortOrder;
         }
     };
 
     /**
-     * updates the compound model for the specified tag to make sure the custom
-     * sort order is defined for the specified tag
+     * Updates the compound model for the specified tag to make sure the custom
+     * sort order is defined for the specified tag.
      *
      * This is needed before any items that use this tag can be saved so that 
-     * the sort orders are correct when they are queried by tag
+     * the sort orders are correct when they are queried by tag.
      */
     Tag.prototype.updateModel = function () {
         Tag.updateModel(this);
     };
 
     /**
-     * rebuilds the index for this tag. This should be used if the sort order
-     * for this tag has been modified
+     * Rebuild the index for this tag. This should be used if the sort order
+     * for this tag has been modified.
      */
     Tag.prototype.rebuildIndex = function () {
         Tag.rebuildIndex(this);
     };
 
     /**
-     * rebuilds the index for this tag. This should be used if the sort order
-     * for this tag has been modified
+     * Rebuilds the index for this tag. This should be used if the sort order
+     * for this tag has been modified.
      * 
-     * @param  {Tag} tag [tag to rebuild index for]
+     * @param  {Tag} tag - tag to rebuild index for
      */
     Tag.rebuildIndex = function (tag) {
         //make sure the custom sort is correct in the schema
@@ -82,28 +87,37 @@ module.exports = function (compound, Tag) {
     };
 
     /**
-     * gets the filter function for this tag
+     * Get the filter function for this tag.
      *
-     * a filter function is used to automatically tag objects based on a 
-     * javascript An example filter function could be as follows:
+     * A filter function is used to automatically tag objects based on a 
+     * javascript. The filter is contained as a string within this.filter
+     * property and the signature is always function (obj) { ... }.
      *
-     * // add all content with more than 5 likes to this tag
-     * function (obj) {
-     *     return obj.likesTotal > 5;
-     * }
+     * obj is a database entity of any type which has a 'tags' property.
+     *
+     * Examples:
+     *
+     *     // Here we are checking a content entity (obj) to see whether it has 
+     *     // more than 5 likes. We are returning true so that the 'popular' 
+     *     // tag is added to it's list of tags.
+     *     function (obj) {
+     *         return obj.likesTotal > 5;
+     *     }
      * 
-     * @return {Function} [the filter function (if any)]
+     * @return {Function} - the filter function (if any)
      */
     Tag.prototype.filterFn = function () {
-        if (!this.filter) throw new Error('No filter defined');
+        if (!this.filter) {
+            throw new Error('No filter defined');
+        }
         return new Function('obj', this.filter);
     };
 
     /**
-     * gets whether an object matches this tag's filter function
+     * Get whether an object matches this tag's filter function.
      * 
-     * @param  {object}   obj  [object to test]
-     * @return {Boolean}       [true or false]
+     * @param  {object}   obj -object to test
+     * @return {Boolean}       true or false
      */
     Tag.prototype.matchFilter = function (obj) {
         if (!this.filter) {
@@ -113,10 +127,10 @@ module.exports = function (compound, Tag) {
     };
 
     /**
-     * updates the count for this tag by querying the database and caching the 
-     * result
+     * Update the count for this tag by querying the database and caching the 
+     * result.
      * 
-     * @param  {Function} callback [callback function]
+     * @param  {Function} callback - callback function
      */
     Tag.prototype.updateCount = function (callback) {
         compound.models[this.type].count({ tags: this.name }, function (err, count) {
@@ -126,47 +140,57 @@ module.exports = function (compound, Tag) {
     };
 
     /**
-     * updates the counts for all tags (within a specific group/type - optional)
+     * Update the counts for all tags (within a specific group/type - optional).
      *
-     * @params {String}   type       [type of tag to update]
-     * @param  {Number}   groupId    [id of group - optional]
-     * @param  {Function} callback   [callback function]
+     * @params {String}   type       - type of tag to update
+     * @param  {Number}   groupId    - id of group - optional
+     * @param  {Function} callback   - callback function
      */
     Tag.updateCounts = function (type, groupId, callback) {
         var cond = {};
-        if(type) cond.type = type;
-        if(groupId) cond.groupId = groupId;
+        if (type) {
+            cond.type = type;
+        }
+        if (groupId) {
+            cond.groupId = groupId;
+        }
 
         var query = {};
-        if(Object.keys(cond).length > 0) query.where = cond;
+        if (Object.keys(cond).length > 0) {
+            query.where = cond;
+        }
 
         Tag.all(query, function (err, tags) {
-            async.forEach(tags, function(tag, next) {
+            async.forEach(tags, function (tag, next) {
                 tag.updateCount(next);
             }, function (err, results) {
-                if(callback) callback();
+                if (callback) {
+                    callback();
+                }
             });
         });
     };
 
     /**
-     * gets all of the matching tags for the specified object by running the
-     * tag filter functions to find matches
+     * Get all of the matching tags for the specified object by running the
+     * tag filter functions (see above) to find matches.
      * 
-     * @param  {object}     obj      [object to get matching tags for]
-     * @param  {Function}   callback [callback function]
+     * @param  {object}     obj      - object to get matching tags for
+     * @param  {Function}   callback - callback function
      */
     Tag.getMatchingTags = function (obj, callback) {
         Tag.all({ where: { type: obj.constructor.modelName }}, function (err, tags) {
             var matchingTags = [];
 
-            console.log(tags);
-
             tags.forEach(function (tag) {
                 // skip group specific tags for other groups
-                if (tag.groupId && tag.groupId != obj.groupId) return;
+                if (tag.groupId && tag.groupId != obj.groupId) {
+                    return;
+                }
 
-                if (tag.matchFilter(obj)) matchingTags.push(tag);
+                if (tag.matchFilter(obj)) {
+                    matchingTags.push(tag);
+                }
             });
 
             callback(err, matchingTags);
@@ -174,16 +198,18 @@ module.exports = function (compound, Tag) {
     };
 
     /**
-     * applies matching tags to this object by running the
-     * tag filter functions to find matches
+     * Apply matching tags to this object by running the
+     * tag filter functions to find matches (see above).
      * 
-     * @param  {object}     obj      [object to apply matching tags for]
-     * @param  {Function}   callback [callback function]
+     * @param  {object}     obj      - object to apply matching tags for
+     * @param  {Function}   callback - callback function
      */
     Tag.applyMatchingTags = function (obj, callback) {
-        Tag.getMatchingTags(obj, function(err, tags) {
+        Tag.getMatchingTags(obj, function (err, tags) {
             tags.forEach(function (tag) {
-                if (!obj.tags[tag.name]) obj.tags.push(tag.name);
+                if (!obj.tags[tag.name]) {
+                    obj.tags.push(tag.name);
+                }
             });
 
             callback(err, obj);
