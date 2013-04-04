87ce88ba9e8960e679894506f481896339c4c199
Marcus Greenwood <marcusgreenwood@Marcuss-MacBook-Air.local>
2013-04-04 10:40:31 +0100

    added caching on stylesheet

--- hatch_modules/stylesheet/app/controllers/stylesheet_controller.js
+++ hatch_modules/stylesheet/app/controllers/stylesheet_controller.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 
 action(function show() {
     header('Content-Type', 'text/css; charset=UTF-8');
+    header('Cache-Control', 'public, max-age=' + (31557600000));
 
     send(fs.readFileSync(app.parent.parent.root + '/public/stylesheets/theme-cerulean.css'));
 });
