57e965839be6a6d5951406dda6dd245053236373
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-05-03 23:53:07 +0400

    Fix route for widget updating, closes #22

--- hatch_modules/admin/config/routes.js
+++ hatch_modules/admin/config/routes.js
@@ -103,7 +103,6 @@ exports.routes = function (map) {
 
     // wildcard to catch all other widget actions
     map.post('/widget/:widgetId/:action', 'widgets');
-    map.post('/widget/:widgetId/:action', 'widgets');
-    map.get('/widget/:widgetId/:action', 'widgets');
+    map.put('/widget/:widgetId/:action', 'widgets');
     map.get('/widget/:widgetId/:action', 'widgets');
 };
