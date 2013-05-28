8e41764deec3548937228fb8eec99f28465e247b
Chris Roebuck <tophtfc@gmail.com>
2013-05-28 20:44:59 +0100

    add different database for dev/test

--- config/database.js
+++ config/database.js
@@ -2,10 +2,12 @@ module.exports =
   { "development":
     { "driver":   "redis-hq"
     , "prefix":   "dev"
+    , "database": 0
     }
   , "test":
     { "driver":   "redis-hq"
     , "prefix":   "test"
+    , "database": 1
     }
   , "production":
     { "driver":   "redis-hq"
