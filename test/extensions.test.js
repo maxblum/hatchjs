f462f15c4be769be14e323c8c5d200011512f644
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-05-29 21:18:59 +0400

    Test extensions, fix issues

---
+++ test/extensions.test.js
@@ -0,0 +1,157 @@
+// This test written using mocha and should. Run it using `make test` command.
+
+var should = require('./'), app, compound, hatch;
+
+var group;
+
+describe('extensions /controller/', function() {
+
+    before(function(done) {
+        app = getApp(function() {
+            hatch = app.compound.hatch;
+            hatch.loadModules(__dirname + '/fixtures/modules');
+            app.compound.structure.controllers.test = TestController;
+            app.compound.models.Group.findOne(function(err, g) {
+                group = g;
+                done();
+            });
+        });
+    });
+
+    describe('moduleEnabled', function() {
+
+        it('should be defined on controller context', test(function(c) {
+            should.exist(c.moduleEnabled);
+            c.moduleEnabled.should.be.a('function');
+            c.next();
+        }));
+
+        it('should return config when module enabled', test(function(c) {
+            c.moduleEnabled('not exist').should.be.false;
+            c.locals.group = group;
+            c.moduleEnabled('admin').should.be.ok;
+            c.next();
+        }));
+
+        it('should return false when module not enabled', test(function(c) {
+            c.locals.group = group;
+            c.moduleEnabled('adminka').should.not.be.ok;
+                c.next();
+        }));
+
+        it('should return false when module not enabled', test(function(c) {
+            delete c.locals.group;
+            c.moduleEnabled('not exist').should.be.false;
+            c.next();
+        }));
+
+    });
+
+    describe('moduleConfigured', function() {
+
+        it('should be defined on controller context', test(function(c) {
+            should.exist(c.moduleConfigured);
+            c.moduleConfigured.should.be.a('function');
+            c.next();
+        }));
+
+        it('should return false when module is not enabled', test(function(c) {
+            c.moduleConfigured('adminka').should.be.false;
+            c.next();
+        }));
+
+        it('should return true when module not configurable', test(function(c) {
+            c.locals.group = group;
+            hatch.modules.admin = {info: {}};
+            c.moduleConfigured('admin').should.be.true;
+            c.next();
+        }));
+
+        it('should return false when module not configured or configuration is not valid', test(function(c) {
+            c.locals.group = group;
+            hatch.modules.admin = {info:{settings:{fields:{a:{required: true}}}}};
+            c.moduleConfigured('admin').should.be.false;
+            c.next();
+        }));
+
+        it('should return true when module configured ok', test(function(c) {
+            c.locals.group = group;
+            hatch.modules.admin = {info:{settings:{fields:{a:{required: true}}}}};
+            c.moduleEnabled('admin').contract = {a: 1};
+            c.moduleConfigured('admin').should.be.true;
+            c.next();
+        }));
+
+    });
+
+    describe('pathFor', function() {
+
+        it('should be defined on controller context', test(function(c) {
+            should.exist(c.pathFor);
+            c.next();
+        }));
+
+        it('should return path helpers collection for module', test(function(c) {
+            hatch.modules.simple.compound.map.resources('users');
+            should.exist(c.pathFor('simple').users);
+            c.pathFor('simple').users().should.equal('/do/simple/users');
+            c.next();
+        }));
+
+        it('should return empty object for not existing module', test(function(c) {
+            delete hatch.modules.admin;
+            JSON.stringify(c.pathFor('admin')).should.equal('{}');
+            c.next();
+        }));
+
+    });
+
+    describe('stripHtml', function() {
+
+        it('should be defined on controller context', test(function(c) {
+            should.exist(c.stripHtml);
+            c.next();
+        }));
+
+        it('should return string without html', test(function(c) {
+            c.stripHtml('hello <b attr="param">world</b>').should.equal('hello  world');
+            c.next();
+        }));
+
+        it('should return text with specified length without trimming words', test(function(c) {
+            c.stripHtml('hello lorem <i>ipsum</i>', 10).should.have.lengthOf(8);
+            c.stripHtml('hello lorem', 10).should.have.lengthOf(8);
+            c.stripHtml('hellolo lolo', 10).should.have.lengthOf(10);
+            c.stripHtml('hellolololo', 10).should.have.lengthOf(13);
+            c.next();
+        }));
+
+    });
+
+    describe('renderContent', function() {
+
+        it('should be defined on controller context', test(function(c) {
+            should.exist(c.renderContent);
+            c.next();
+        }));
+
+        it('should be defined on controller context', test(function(c) {
+            c.renderContent('ha');
+            c.next();
+        }));
+
+    });
+
+});
+
+function test(fn) {
+    return function (done) {
+        TestController.test = fn;
+        app.compound.controllerBridge.callControllerAction('test', 'action', {pagePath: ''}, {}, done);
+    };
+}
+
+function TestController(){}
+TestController.prototype.action = function(c) {
+    TestController.test.call(this, c);
+};
