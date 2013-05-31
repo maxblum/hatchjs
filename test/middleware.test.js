b9bb8972e4b332fe8cbc1d7b86444bebeb22f3df
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-05-31 22:28:08 +0400

    Error handling middleware

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -14,7 +14,7 @@ describe('middleware', function() {
     });
 
     function middleware(req, res, next) {
-        tester(req);
+        tester(req, res, next);
     }
 
     function remove() {
@@ -65,16 +65,98 @@ describe('middleware', function() {
 
     });
 
-    function test(url, callback) {
+    describe('error', function() {
+
+        before(function() {
+            compound.injectMiddlewareAfter('hatchMiddleware', middleware);
+        });
+
+        after(remove);
+
+        it('should send 404 error in json', test('/', function(req, res, n) {
+            req.params = {format: 'json'};
+            n(new hatch.errors.NotFound(req, 'custom message'));
+        }, function(err, c) {
+            c.text.should.equal(JSON.stringify({
+                error: {
+                    code: 404,
+                    name: 'Not Found',
+                    message: 'custom message'
+                }
+            }));
+            c.res.headers['content-type'].should.equal('application/json; charset=utf-8');
+            c.res.statusCode.should.equal(404);
+        }));
+
+        it('should send 500 error in json', test('/', function(req, res, n) {
+            req.params = {format: 'json'};
+            var hello = world;
+        }, function(err, c) {
+            c.text.should.equal(JSON.stringify({
+                error: {
+                    code: 500,
+                    name: 'ReferenceError',
+                    message: 'world is not defined'
+                }
+            }));
+            c.res.headers['content-type'].should.equal('application/json; charset=utf-8');
+            c.res.statusCode.should.equal(500);
+        }));
+
+        it('should send 404 error in html with status 404', test('/', function(req, res, n) {
+            req.params = {format: 'html'};
+            n(new hatch.errors.NotFound(req, 'custom message'));
+        }, function(err, c) {
+            c.res.headers['content-type'].should.equal('text/html; charset=utf-8');
+            c.res.statusCode.should.equal(404);
+        }));
+
+        it('should send 500 error in html with status 500', test('/', function(req, res, n) {
+            req.params = {format: 'html'};
+            n(new hatch.errors.InternalError('custom message'));
+        }, function(err, c) {
+            c.res.headers['content-type'].should.equal('text/html; charset=utf-8');
+            c.res.statusCode.should.equal(500);
+        }));
+
+        it('should send 403 error in html with status 403', test('/', function(req, res, n) {
+            req.params = {format: 'html'};
+            n(new hatch.errors.Forbidden('custom message'));
+        }, function(err, c) {
+            c.res.headers['content-type'].should.equal('text/html; charset=utf-8');
+            c.res.statusCode.should.equal(403);
+        }));
+
+        it.skip('should render custom error using group special page', test('/', function(req, res, n) {
+            var customError = new Error('hello world');
+            customError.code = 501;
+            compound.models.Group.find(1, function(err, group) {
+                req.group = group;
+                n(customError);
+            });
+        }, function(err, c) {
+            // c.res.headers['content-type'].should.equal('text/html; charset=utf-8');
+            c.res.statusCode.should.equal(501);
+        }));
+
+    });
+
+    function test(url, callback, after) {
         return function(done) {
             getClient(app).get(url).end(function(err, c) {
                 if (err) {
                     console.log(err);
                 }
+                if (after) {
+                    after(err, c);
+                    done();
+                }
             });
-            tester = function(req) {
-                callback(req);
-                done();
+            tester = function(req, res, next) {
+                callback(req, res, next);
+                if (!after) {
+                    done();
+                }
             };
         };
     }
