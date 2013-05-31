e7da48468e1bb6a95e1109df56ed6664214f83f3
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-05-31 22:26:57 +0400

    Split databases for dev and test

--- config/database.js
+++ config/database.js
@@ -2,10 +2,12 @@ module.exports =
   { "development":
     { "driver":   "redis-hq"
     , "prefix":   "dev"
+    , "database": 1
     }
   , "test":
     { "driver":   "redis-hq"
     , "prefix":   "test"
+    , "database": 2
     }
   , "production":
     { "driver":   "redis-hq"
