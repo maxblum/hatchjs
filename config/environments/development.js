1b2308626fa5e28ae80e65d6727ffe31ddc8959d
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-05-31 12:14:57 +0400

    Remove env info from dev env

--- config/environments/development.js
+++ config/environments/development.js
@@ -5,7 +5,6 @@ module.exports = function (compound) {
 
     app.configure('development', function () {
         app.enable('log actions');
-        app.enable('env info');
         app.enable('watch');
         app.use(require('express').errorHandler({ dumpExceptions: true, showStack: true }));
     });
