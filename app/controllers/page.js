049ee031ca46d01bf69510ec10a504bc7db227bc
Marcus Greenwood <marcusgreenwood@Marcuss-MacBook-Air.local>
2013-06-13 12:45:11 +0100

    removing leading and trailing slashes from route to fix intermittent bug

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -11,11 +11,15 @@ require('util').inherits(PageController, Application);
 PageController.prototype.show = function (c) {
     var Page = c.Page;
 
+    // remove trailing and leading slashes
+    c.req.url = c.req.url.replace(/^\/|\/$/g, '');
+
     this.group.definePage(decodeURIComponent(c.req.url), c, function render(err, page) {
         if (err || !page) {
             return c.next(err);
         }
         c.req.page = page;
+
         page.renderHtml(c.req, function (err, html) {
             if (err) {
                 return c.next(err);
