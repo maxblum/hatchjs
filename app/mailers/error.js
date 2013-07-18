a6b27a5276baa3b0f5b6afa68853724b99d8bba2
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-07-18 19:12:35 +0400

    Error reporting fix

--- app/mailers/error.js
+++ app/mailers/error.js
@@ -1,8 +1,8 @@
 
-exports.notify = function(err, settings) {
+exports.notify = function(err, settings, done) {
     this.locals.err = err;
     this.send({
         to: settings.email,
         subject: (settings.subject || 'Hatch Error: ') + err.message
-    });
+    }, done);
 };
