8be323fe088b8f1b93c3cd96e23593302ba9cc43
Mark S. Everitt <marke@entrago.com>
2013-09-17 11:31:54 +0100

    New Author column in the moderation pane.

--- app/models/Comment.js
+++ app/models/Comment.js
@@ -1,18 +1,18 @@
 //
-// Hatch.js is a CMS and social website building framework built in Node.js 
+// Hatch.js is a CMS and social website building framework built in Node.js
 // Copyright (C) 2013 Inventures Software Ltd
-// 
+//
 // This file is part of Hatch.js
-// 
+//
 // Hatch.js is free software: you can redistribute it and/or modify it under the terms of the
 // GNU General Public License as published by the Free Software Foundation, version 3
-// 
+//
 // Hatch.js is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// 
+//
 // See the GNU General Public License for more details. You should have received a copy of the GNU
 // General Public License along with Hatch.js. If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // Authors: Marcus Greenwood, Anatoliy Chakkaev and others
 //
 
@@ -24,7 +24,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * Get whether this comment has been flagged.
-     * 
+     *
      * @return {Boolean}
      */
     Comment.getter.hasFlag = function () {
@@ -33,7 +33,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * Set the date automatically if we don't have one.
-     * 
+     *
      * @param  {Function} next - callback function
      */
     Content.beforeSave = function (next) {
@@ -45,7 +45,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * After a comment is saved, update the parent content item.
-     * 
+     *
      * @param  {Function} next - callback function
      */
     Comment.afterSave = function (next) {
@@ -54,7 +54,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * After a comment is deleted, update the parent content item.
-     * 
+     *
      * @param  {Function} next - callback function
      */
     Comment.afterDestroy = function (next) {
@@ -63,7 +63,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * Like or unlike a comment.
-     * 
+     *
      * @param  {User}     user     - user performing the like
      * @param  {Function} callback - callback function
      */
@@ -83,7 +83,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * Flag or unflag a comment.
-     * 
+     *
      * @param  {User}     user     - user performing the flag
      * @param  {Function} callback - callback function
      */
@@ -112,7 +112,7 @@ module.exports = function (compound, Comment) {
 
     /**
      * Delete this post and blacklist the author from the group it was posted to.
-     * 
+     *
      * @param  {Function} callback - call back function
      */
     Content.prototype.destroyAndBan = function (callback) {
